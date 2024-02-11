04a512ec1893b71980e251081561d44557233931
dannye <33dannye@gmail.com>
2024-02-11 17:40:13 -0600

    example song: add loops and calls

--- example/crystaltracked.asm
+++ example/crystaltracked.asm
@@ -9,7 +9,6 @@ Music_CrystalTracked_Ch1:
 	volume 7, 7
 	note_type 12, 15, 8
 .mainLoop:
-	volume_envelope 10, 8
 	duty_cycle 0
 	note_type 12, 10, 8
 	octave 3
@@ -77,31 +76,7 @@ Music_CrystalTracked_Ch1:
 	note C#, 4
 	note_type 12, 10, 8
 	octave 4
-	note B_, 1
-	note G_, 1
-	octave 5
-	note C_, 1
-	octave 4
-	note B_, 1
-	note G_, 1
-	note E_, 1
-	note C_, 1
-	octave 3
-	note G_, 1
-	octave 4
-	note C_, 1
-	rest 1
-	note D_, 1
-	rest 1
-	octave 3
-	note B_, 1
-	octave 4
-	note C_, 1
-	note D_, 1
-	note E_, 1
-	note D_, 1
-	rest 1
-	note B_, 1
+	sound_call .sub1
 	rest 3
 	duty_cycle 2
 	note E_, 1
@@ -192,31 +167,9 @@ Music_CrystalTracked_Ch1:
 	note B_, 4
 	note_type 12, 5, 8
 	octave 4
-	note B_, 1
-	note G_, 1
-	octave 5
-	note C_, 1
+	sound_call .sub1
+	note_type 12, 5, 8
 	octave 4
-	note B_, 1
-	note G_, 1
-	note E_, 1
-	note C_, 1
-	octave 3
-	note G_, 1
-	octave 4
-	note C_, 1
-	rest 1
-	note D_, 1
-	rest 1
-	octave 3
-	note B_, 1
-	octave 4
-	note C_, 1
-	note D_, 1
-	note E_, 1
-	note D_, 1
-	rest 1
-	note B_, 1
 	rest 5
 	note D_, 1
 	note D#, 1
@@ -250,6 +203,34 @@ Music_CrystalTracked_Ch1:
 	octave 8
 	sound_loop 0, .mainLoop
 
+.sub1:
+	note B_, 1
+	note G_, 1
+	octave 5
+	note C_, 1
+	octave 4
+	note B_, 1
+	note G_, 1
+	note E_, 1
+	note C_, 1
+	octave 3
+	note G_, 1
+	octave 4
+	note C_, 1
+	rest 1
+	note D_, 1
+	rest 1
+	octave 3
+	note B_, 1
+	octave 4
+	note C_, 1
+	note D_, 1
+	note E_, 1
+	note D_, 1
+	rest 1
+	note B_, 1
+	sound_ret
+
 Music_CrystalTracked_Ch2:
 	note_type 12, 15, 8
 .mainLoop:
@@ -257,55 +238,7 @@ Music_CrystalTracked_Ch2:
 	octave 2
 	duty_cycle 1
 	vibrato 0, 2, 2
-	note G#, 2
-	octave 3
-	note C_, 2
-	note D#, 1
-	rest 1
-	note C_, 1
-	octave 2
-	rest 1
-	note G#, 2
-	octave 3
-	note D#, 2
-	note F_, 1
-	rest 1
-	note C_, 1
-	octave 2
-	rest 1
-	note G#, 2
-	octave 3
-	note C_, 2
-	note D#, 1
-	rest 1
-	note C_, 1
-	octave 2
-	rest 1
-	note G#, 2
-	octave 3
-	note D#, 2
-	note G#, 1
-	rest 1
-	note C_, 1
-	octave 2
-	rest 1
-	vibrato 0, 4, 2
-	note G#, 2
-	octave 3
-	note C_, 2
-	note F_, 1
-	rest 1
-	note C_, 1
-	octave 2
-	rest 1
-	note F_, 2
-	octave 3
-	note C_, 2
-	note F_, 1
-	rest 1
-	octave 2
-	note B_, 1
-	rest 1
+	sound_call .sub1
 	note A_, 2
 	octave 3
 	note C_, 2
@@ -320,25 +253,7 @@ Music_CrystalTracked_Ch2:
 	rest 1
 	note G_, 1
 	rest 1
-	vibrato 0, 2, 2
-	note C_, 2
-	note G_, 2
-	note B_, 1
-	rest 1
-	note G_, 1
-	rest 1
-	note C_, 2
-	note G_, 2
-	note A#, 1
-	rest 1
-	note G_, 1
-	rest 1
-	note C_, 2
-	note G_, 2
-	note A_, 1
-	rest 1
-	note G_, 1
-	rest 1
+	sound_call .sub2
 	note C_, 2
 	note G_, 2
 	vibrato 0, 5, 1
@@ -355,10 +270,7 @@ Music_CrystalTracked_Ch2:
 	octave 3
 	note C_, 2
 	volume_envelope 15, 8
-	note G#, 2
-	note G_, 2
-	note F_, 2
-	note E_, 2
+	sound_call .sub3
 	rest 2
 	octave 2
 	volume_envelope 12, 8
@@ -375,6 +287,57 @@ Music_CrystalTracked_Ch2:
 	vibrato 0, 1, 1
 	duty_cycle 2
 	volume_envelope 10, 8
+	sound_call .sub1
+	octave 2
+	note_type 12, 10, 8
+	note G#, 2
+	octave 3
+	note D#, 2
+	note F_, 1
+	rest 1
+	note C_, 1
+	octave 2
+	rest 1
+	note F_, 2
+	note G_, 2
+	note A_, 1
+	rest 1
+	note G_, 1
+	rest 1
+	sound_call .sub2
+	octave 2
+	note_type 12, 10, 8
+	note C_, 2
+	note G_, 2
+	note G#, 1
+	rest 1
+	note F#, 1
+	octave 1
+	rest 1
+	duty_cycle 0
+	note G#, 2
+	octave 2
+	note D#, 2
+	note G#, 2
+	octave 3
+	note C_, 2
+	sound_call .sub3
+	note_type 12, 10, 8
+	rest 2
+	octave 2
+	note D#, 2
+	note G_, 2
+	octave 3
+	note C_, 2
+	note E_, 2
+	note D#, 2
+	note C#, 2
+	octave 2
+	note A#, 2
+	octave 8
+	sound_loop 0, .mainLoop
+
+.sub1:
 	note G#, 2
 	octave 3
 	note C_, 2
@@ -405,8 +368,8 @@ Music_CrystalTracked_Ch2:
 	note G#, 1
 	rest 1
 	note C_, 1
-	rest 1
 	octave 2
+	rest 1
 	note G#, 2
 	octave 3
 	note C_, 2
@@ -423,20 +386,9 @@ Music_CrystalTracked_Ch2:
 	octave 2
 	note B_, 1
 	rest 1
-	note G#, 2
-	octave 3
-	note D#, 2
-	note F_, 1
-	rest 1
-	note C_, 1
-	octave 2
-	rest 1
-	note F_, 2
-	note G_, 2
-	note A_, 1
-	rest 1
-	note G_, 1
-	rest 1
+	sound_ret
+
+.sub2:
 	note C_, 2
 	note G_, 2
 	note B_, 1
@@ -455,71 +407,33 @@ Music_CrystalTracked_Ch2:
 	rest 1
 	note G_, 1
 	rest 1
-	note C_, 2
-	note G_, 2
-	note G#, 1
-	rest 1
-	note F#, 1
-	octave 1
-	note_type 12, 10, 8
-	rest 1
-	duty_cycle 0
-	note G#, 2
-	octave 2
-	note D#, 2
-	note G#, 2
-	octave 3
-	note C_, 2
+	sound_ret
+
+.sub3:
 	note G#, 2
 	note G_, 2
 	note F_, 2
 	note E_, 2
-	rest 2
-	octave 2
-	note D#, 2
-	note G_, 2
-	octave 3
-	note C_, 2
-	note E_, 2
-	note D#, 2
-	note C#, 2
-	octave 2
-	note A#, 2
-	octave 8
-	sound_loop 0, .mainLoop
+	sound_ret
 
 Music_CrystalTracked_Ch3:
 	note_type 12, 1, 0
 .mainLoop:
-	volume_envelope 1, 7
 	octave 2
-	note G#, 12
-	note D#, 4
-	note G#, 8
-	note F#, 8
-	note F_, 4
-	note C_, 8
 	note_type 12, 1, 7
-	octave 1
-	note G#, 2
-	note G_, 2
-	note F_, 6
-	note G#, 2
+	sound_call .sub1
 	note_type 8, 1, 7
 	note G_, 4
 	note F_, 4
 	note C#, 4
 	note_type 12, 1, 7
+.loop1:
 	note C_, 8
 	note G_, 2
 	note F_, 2
 	note E_, 1
 	rest 3
-	note C_, 8
-	note G_, 2
-	note F_, 2
-	note E_, 1
-	rest 3
+	sound_loop 2, .loop1
 	octave 2
 	volume_envelope 1, 4
 	note C_, 8
@@ -535,33 +449,20 @@ Music_CrystalTracked_Ch3:
 	note G_, 2
 	octave 2
 	volume_envelope 1, 6
-	note G#, 12
-	note D#, 4
-	note G#, 8
-	note F#, 8
-	note F_, 4
-	note C_, 8
+	sound_call .sub1
 	octave 1
-	note G#, 2
-	note G_, 2
-	note F_, 6
-	note G#, 2
-	note_type 8, 1, 7
-	volume_envelope 1, 6
+	note_type 8, 1, 6
 	note G_, 4
 	note F_, 4
 	note C#, 4
 	note_type 12, 1, 7
+.loop2:
 	note C_, 8
 	note G_, 2
 	note F_, 2
 	note E_, 1
 	rest 3
-	note C_, 8
-	note G_, 2
-	note F_, 2
-	note E_, 1
-	rest 3
+	sound_loop 2, .loop2
 	volume_envelope 1, 8
 	note C_, 1
 	rest 1
@@ -588,3 +489,17 @@ Music_CrystalTracked_Ch3:
 	note F#, 2
 	octave 8
 	sound_loop 0, .mainLoop
+
+.sub1:
+	note G#, 12
+	note D#, 4
+	note G#, 8
+	note F#, 8
+	note F_, 4
+	note C_, 8
+	octave 1
+	note G#, 2
+	note G_, 2
+	note F_, 6
+	note G#, 2
+	sound_ret
