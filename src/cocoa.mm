f08464b7d7abfd3d8336d08eb26be2707dbd4099
dannye <33dannye@gmail.com>
2024-10-29 17:08:56 -0500

    Remove unused mac code

--- src/cocoa.mm
+++ src/cocoa.mm
@@ -6,28 +6,6 @@
 
 #include "cocoa.h"
 
-void cocoa_set_window_transparency(const Fl_Window *w, double alpha) {
-	[fl_xid(w) setAlphaValue:alpha];
-}
-
-bool cocoa_is_maximized(const Fl_Window *w) {
-	return [fl_xid(w) isZoomed];
-}
-
-void cocoa_maximize(const Fl_Window *w) {
-	[fl_xid(w) setFrame:[[fl_xid(w) screen] frame] display:true];
-}
-
-bool cocoa_is_fullscreen(const Fl_Window *w) {
-	return ([fl_xid(w) styleMask] & NSWindowStyleMaskFullScreen) == NSWindowStyleMaskFullScreen;
-}
-
-void cocoa_fullscreen(const Fl_Window *w, bool state) {
-	if (state != cocoa_is_fullscreen(w)) {
-		[fl_xid(w) toggleFullScreen:nil];
-	}
-}
-
 void cocoa_set_appearance(const Fl_Window *w, enum cocoa_appearance appearance_id) {
 	NSAppearance *appearance;
 	switch (appearance_id) {
